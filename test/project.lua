ade90c06325175d04e278b91bd87cf147c818349
rrhiemstar <rrhiemstar@gmail.com>
2024-01-15 23:21:21 +0100

    <test> added integration tests that utilize github cli

--- test/project.lua
+++ test/project.lua
@@ -10,38 +10,45 @@ function testIsProjTable()
 end
 
 function testPkgCreate()
-    Proj.create("MyPackage")
-    lu.assertTrue(Proj.ispkg("MyPackage"))
-    os.execute("rm -rf MyPackage")
-    lu.assertFalse(Proj.ispkg("MyPackage"))
+    Proj.create("MyPackage", "tmp")
+    lu.assertTrue(Proj.ispkg("tmp/MyPackage"))
+    os.execute("rm -rf tmp")
+    lu.assertFalse(Proj.ispkg("tmp/MyPackage"))
     lu.assertFalse(Proj.ispkg(Proj.terrahome.."/dev/"))
 end
 
 function testReadWriteProjfile()
-  Proj.create("MyPackage")
-  local table1 = require("MyPackage.Project")
-  Proj.save(table1, "tmpProject.lua", "MyPackage")
-  local table2 = require("MyPackage.tmpProject")
+  Proj.create("MyPackage", "tmp")
+  local table1 = require("tmp.MyPackage.Project")
+  Proj.save(table1, "tmpProject.lua", "tmp/MyPackage")
+  local table2 = require("tmp.MyPackage.tmpProject")
 
   lu.assertEquals(table1.name, "MyPackage")
   lu.assertEquals(table2.name, "MyPackage")
   lu.assertEquals(table1.uuid, table2.uuid)
-  os.execute("rm -rf MyPackage")
+  os.execute("rm -rf tmp")
+end
+
+local function create_pkg(pkgname, path)
+  local root = path.."/"..pkgname
+  Proj.create(pkgname, path)
+  Cm.throw{cm="gh repo create "..pkgname.." --public"}
+  Cm.throw{cm="git remote add origin git@github.com:renehiemstra/"..pkgname..".git", root=root}
+  Cm.throw{cm="git push --set-upstream origin main", root=root}
+end
+
+local function delete_pkg(pkgname, path)
+  local root = path.."/"..pkgname
+  Cm.throw{cm="gh repo delete "..pkgname.." --yes"}
+  Cm.throw{cm="rm -rf "..path}
 end
 
 function testPkgCreateAndClone()
-  Proj.create("PkgExample")
-  Cm.throw{cm="gh repo create PkgExample --public"}
-  Cm.throw{cm="git remote add origin git@github.com:renehiemstra/PkgExample.git", root="PkgExample"}
-  Cm.throw{cm="git push --set-upstream origin main", root="PkgExample"}
-
-  local status, err = pcall(Proj.clone, {root="tmp", url="git@github.com:renehiemstra/PkgExample.git"})
+  create_pkg("ExamplePkg", "tmp1")
+  local status, err = pcall(Proj.clone, {root="tmp2", url="git@github.com:renehiemstra/ExamplePkg.git"})
   lu.assertTrue(status)
-
-  --cleanup
-  Cm.throw{cm="gh repo delete PkgExample --yes", root="PkgExample"}
-  Cm.throw{cm="rm -rf tmp"}
-  Cm.throw{cm="rm -rf PkgExample"}
+  Cm.throw{cm="rm -rf tmp2"}
+  delete_pkg("ExamplePkg", "tmp1")
 end
 
--- lu.LuaUnit.run()
+lu.LuaUnit.run()
--- test/registry.lua
+++ test/registry.lua
@@ -1,22 +1,90 @@
 local lu = require "luaunit"
 
 local pkgdir = "dev.Pkg.src."
+local Cm = require(pkgdir.."command")
+local Proj = require(pkgdir.."project")
 local Reg = require(pkgdir.."registry")
 
-function testSaveRegistry()
+-- function testSaveRegistry()
 
-    local table1 = require "registries.MyRegistry.Registry"
-    Reg.save(table1, "tmpRegistry.lua", ".")
-    local table2 = require "tmpRegistry"
+--     local table1 = require "registries.MyRegistry.Registry"
+--     Reg.save(table1, "tmpRegistry.lua", ".")
+--     local table2 = require "tmpRegistry"
 
-    lu.assertTrue(table1.name == "MyRegistry" and table2.name=="MyRegistry")
-    lu.assertEquals(table1.uuid, table2.uuid)
-    os.execute("rm tmpRegistry.lua")
+--     lu.assertTrue(table1.name == "MyRegistry" and table2.name=="MyRegistry")
+--     lu.assertEquals(table1.uuid, table2.uuid)
+--     os.execute("rm tmpRegistry.lua")
+-- end
+
+-- function testIsRegistry()
+--     lu.assertTrue(Reg.isreg("MyRegistry"))
+--     lu.assertFalse(Reg.isreg("RegistryDoesntExist"))
+-- end
+
+-- function testRegistryCreate()
+--     Cm.throw{cm="gh repo create ExampleRegistry --public"}
+--     Reg.create{name="ExampleRegistry", url="git@github.com:renehiemstra/ExampleRegistry"}
+--     lu.assertTrue(Reg.isreg("ExampleRegistry"))
+
+--     --cleanup
+--     local root = Reg.regdir.."/ExampleRegistry"
+--     Cm.throw{cm="gh repo delete ExampleRegistry --yes"}
+--     Cm.throw{cm="rm -rf "..Reg.regdir.."/ExampleRegistry"}
+-- end
+
+local function create_pkg(pkgname, path)
+    local root = path.."/"..pkgname
+    Proj.create(pkgname, path)
+    Cm.throw{cm="gh repo create "..pkgname.." --public"}
+    Cm.throw{cm="git remote add origin git@github.com:renehiemstra/"..pkgname..".git", root=root}
+    Cm.throw{cm="git push --set-upstream origin main", root=root}
 end
 
-function testIsRegistry()
-    lu.assertTrue(Reg.isreg("MyRegistry"))
-    lu.assertFalse(Reg.isreg("RegistryDoesntExist"))
+local function delete_pkg(pkgname, path)
+    if pkgname~="Pkg" then --procaution
+        local root = path.."/"..pkgname
+        Cm.throw{cm="gh repo delete "..pkgname.." --yes"}
+        Cm.throw{cm="rm -rf "..path}
+    end
 end
 
--- lu.LuaUnit.run()
+local function create_reg(regname)
+    Cm.throw{cm="gh repo create "..regname.." --public"}
+    Reg.create{name=regname, url="git@github.com:renehiemstra/"..regname}
+end
+
+local function delete_reg(regname)
+    Cm.throw{cm="gh repo delete "..regname.." --yes"}
+    Cm.throw{cm="rm -rf "..Reg.regdir.."/"..regname}
+end
+
+function testRegistryAddPkg()
+    --create registry
+    local registry = "ExampleRegistry"
+    create_reg(registry)
+    lu.assertTrue(Reg.isreg(registry))
+
+    --create packages
+    local projectdir = Proj.terrahome.."/clones"
+    local packages = {"ExamplePkg", "ExampleDep1", "ExampleDep2", "ExampleDepDep"}
+    for _, pkg in pairs(packages) do
+        create_pkg(pkg, projectdir)
+    end
+
+    --register packages
+    for _, pkg in pairs(packages) do
+        Reg.register{reg=registry, url="git@github.com:renehiemstra/"..pkg..".git"}
+    end
+
+    --cleanup registry
+    delete_reg(registry)
+    lu.assertFalse(Reg.isreg(registry))
+
+    --cleanup packages
+    for _, pkg in pairs(packages) do
+        delete_pkg(pkg, projectdir)
+        Cm.throw{cm="rm -rf "..Proj.terrahome.."/packages/"..pkg}
+    end
+end
+
+lu.LuaUnit.run()
