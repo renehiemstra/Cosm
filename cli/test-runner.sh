84914e8c506508c40c57ac2cdb7d8416a30cb2d8
rrhiemstar <rrhiemstar@gmail.com>
2024-02-27 15:46:27 +0100

    <test> added test runner for the CLI.

---
+++ cli/test-runner.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+./test-register.sh
+./test-release.sh
+./test-add.sh
+./test-rm.sh
+./test-upgrade.sh
+./test-downgrade.sh
+./test-integration-simple.sh
+./test-integration.sh
